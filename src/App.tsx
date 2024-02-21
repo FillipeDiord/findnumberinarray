5d509cdbc38cc7c333928b4283e27721783806d6
Fillipe Diord <fillipe.diord@hotmail.com>
2024-02-21 17:21:11 -0300

    refactor: 💡 remove class
    remove class

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Home } from './components/Home'
-import { Label } from './components/Label'
 
 import './App.css'
 
 function App() {
   return (
     <>
-      <Label className='font-bold m-10'>Escolha o número inicial e o número final,
-        para que o array seja gerado com números aleatórios
-      </Label>
       <div className='flex items-center justify-center w-[50rem]'>
         <div className='flex-row items-center justify-center'>
           <Home />
