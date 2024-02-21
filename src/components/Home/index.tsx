b5b61e76267111ca8a51da9f61c762be89101e97
Fillipe Diord <fillipe.diord@hotmail.com>
2024-02-21 17:21:58 -0300

    refactor: 💡 adding FindNumber component and validation
    adding FindNumber component and validation

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import { FindNumber } from '../FindNumber';
 import { GenerateArray } from '../GenerateArray';
 
 export function Home() {
+  const [numbersArray, setNumbersArray] = useState<number[]>([]);
+
   return (
     <>
-      <GenerateArray />
+      {numbersArray.length > 0 ?
+        < FindNumber numbersArray={numbersArray} />
+        :
+        <GenerateArray updateNumberArrayState={setNumbersArray} />
+      }
     </>
   )
 }
